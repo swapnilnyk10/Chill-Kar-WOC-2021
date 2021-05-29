557928e4d2db00866aaa6fcb3ac2f237e19ff4d9
Swapnil Nayak <80190115+swapnilnyk10@users.noreply.github.com>
2021-05-29 19:03:39 +0530

    updated

--- Website/settings.py
+++ Website/settings.py
@@ -145,6 +145,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER ='swapnilnayak9@gmail.com'
-EMAIL_HOST_PASSWORD ='zjbugtmyvunisbuz '
 EMAIL_USE_TLS = True
