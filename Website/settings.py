c4f65e22f5c3d21446dd6177bbbe6d1f4c502f8e
Swapnil Nayak <80190115+swapnilnyk10@users.noreply.github.com>
2021-08-23 13:15:26 +0530

    Updates for heroku

--- Website/settings.py
+++ Website/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # STATIC_ROOT =os.path.join(BASE_DIR,'templates')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
